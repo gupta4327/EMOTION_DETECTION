3da06648920613e17f8fafb883d6d49558def22b
Aman Gupta <gupta4327@gmail.com>
2025-06-22 23:39:58 +0530

    fix - v3

--- scripts/load_env_from_yaml.py
+++ scripts/load_env_from_yaml.py
@@ -1,12 +1,11 @@
-# scripts/load_env_from_yaml.py
 import yaml
 import sys
 import os
 
-yaml_file = sys.argv[1]  # e.g., "vars.yml"
+yaml_file = sys.argv[1]
 with open(yaml_file) as f:
     data = yaml.safe_load(f)
     with open(os.environ["GITHUB_ENV"], "a") as env_file:
         for k, v in data.items():
-            line = f'{k}="{v}"' if isinstance(v, str) else f'{k}="{str(v)}"'
-            env_file.write(line + "\n")
+            clean_val = str(v).strip('"')  # removes surrounding quotes, just in case
+            env_file.write(f'{k}="{clean_val}"\n')
